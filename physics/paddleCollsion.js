d053941674f48301cd1a908dcc207d8eba57d998
alowtron <alowtron@gmail.com>
2026-03-31 18:19:50 -0400

    collision seems to be fully working

--- physics/paddleCollsion.js
+++ physics/paddleCollsion.js
@@ -1,9 +1,23 @@
 function paddleCollision(paddle, paddleIndex, ball) {
-  if (paddle.x + paddle.width >= ball.x - ball.size) {
-    if (paddle.y + paddle.height >= ball.y - ball.size && paddle.y - paddle.height <= ball.y + paddle.height) {
-      console.log('bounce')
-      ball.xMove *= -1
-    }
+  const paddleLeft = paddle.x - paddle.width
+  const paddleRight = paddle.x + paddle.width
+  const paddleTop = paddle.y - paddle.height
+  const paddleBottom = paddle.y + paddle.height
+  
+  const ballLeft = ball.x - ball.size
+  const ballRight = ball.x + ball.size
+  const ballTop = ball.y - ball.size
+  const ballBottom = ball.y + ball.size
+
+  if (paddleRight >= ballLeft
+    && paddleTop + 25 <= ballBottom && paddleBottom - 25 >= ballTop
+  ) {
+    ball.xMove *= -1
+  } else if (paddleRight >= ballLeft && paddleLeft <= ballRight 
+    && paddleTop <= ballBottom && paddleBottom >= ballTop
+  ) {
+    ball.xMove *= -1
+    ball.yMove *= -1
     
   }
 }
--- vars.js
+++ vars.js
@@ -16,15 +16,15 @@ function createSinglePlayer() {
         size: 10,
         color: "white",
         speed: 8,
-        xMove: 2,
-        yMove: 2,
+        xMove: -2,
+        yMove: -2,
       }
     ],
     paddles: [
       {
         x: 50,
         y: gameHeight / 2,
-        width: 10,
+        width: 10,// width: 10,
         height: 185,
         speed: 4,
         color: 'white'
