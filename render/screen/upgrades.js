c6ba43db69102b4da38a4612e74e808e58d2aed6
alowtron <alowtron@gmail.com>
2026-04-02 14:44:50 -0400

    started to add in all of the stuff needed to take and display the upgrade choices

--- render/screen/upgrades.js
+++ render/screen/upgrades.js
@@ -1,9 +1,39 @@
+const boxPos = [
+  {
+    x: 50,
+    y: 200
+  },
+  {
+    x: gameWidth / 3 + 50,
+    y: 200
+  },
+  {
+    x: gameWidth / 3 * 2 + 50,
+    y: 200
+  }
+]
+
+const boxSize = {
+  width: 550,
+  height: 830
+}
 function upgrades() {
-  const tempText = "Upgrades here in the future"
+  const upgradeText = "Choose a upgrade"
   ctx.fillStyle = "green"
   ctx.font = "100px Arial"
 
-  ctx.fillText(tempText, gameWidth / 2 - 600, 300)
+  ctx.fillText(upgradeText, gameWidth / 2 - 400, 120)
+
+  ctx.strokeStyle ="white"
+  for (let i = 0; i < 3; i++) {
+    ctx.beginPath()
+    ctx.moveTo(boxPos[i].x, boxPos[i].y)
+    ctx.lineTo(boxPos[i].x, boxPos[i].y + boxSize.height)
+    ctx.lineTo(boxPos[i].x + boxSize.width, boxPos[i].y + boxSize.height)
+    ctx.lineTo(boxPos[i].x + boxSize.width, boxPos[i].y)
+    ctx.lineTo(boxPos[i].x, boxPos[i].y)
+    ctx.stroke()
+  }
 
   if (state.input.player1.escape || state.input.player1.enter) {
     addUpgrade(1)
