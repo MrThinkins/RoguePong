d58aab727e41c3d676916473eaff1b5310385a2f
MrThinkins <147556072+MrThinkins@users.noreply.github.com>
2026-04-06 09:43:31 -0400

    Update addUpgrade.js

--- gameLogic/addUpgrade.js
+++ gameLogic/addUpgrade.js
@@ -1,11 +1,11 @@
 function addUpgrade(index) {
-  console.log('index')
-  console.log(availableUpgrades[index])
+  // console.log('index')
+  // console.log(availableUpgrades[index])
   currentUpgrades.push(availableUpgrades[index])
-  console.log(`currentUpgrades.length: ${currentUpgrades.length}`)
+  // console.log(`currentUpgrades.length: ${currentUpgrades.length}`)
   if (availableUpgrades[index].rarity != "common" ) {
     availableUpgrades.splice(index, 1)
-    console.log(`currentUpgrades.length: ${currentUpgrades.length}`)
+    // console.log(`currentUpgrades.length: ${currentUpgrades.length}`)
   }
   
 }
