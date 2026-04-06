8f5d56cecf05d1d2686c8770202d73a24263853f
alowtron <alowtron@gmail.com>
2026-04-06 17:35:08 -0400

    highlighting is working

--- render/screen/options.js
+++ render/screen/options.js
@@ -10,12 +10,40 @@ function renderOption() {
   ctx.font = "150px Arial"
   ctx.fillText("Options", gameWidth / 2 - 270, 200)
 
+  ctx.fillStyle ="grey"
+  if (state.current.selected == 0) {
+    ctx.fillRect(gameWidth / 2 - 310, 345, 510, 100)
+  } else if (state.current.selected == 1) {
+    ctx.fillRect(gameWidth / 2 - 290, 545, 490, 100)
+  } else if (state.current.selected == 2) {
+    ctx.fillRect(gameWidth / 2 - 120, 745, 190, 100)
+  }
+
+  ctx.fillStyle = "white"
   ctx.font = "75px Arial"
   ctx.fillText("Sound Volume", gameWidth / 2 - 300, 425)
   ctx.fillRect(soundEffectPos.x, soundEffectPos.y, soundEffectPos.xEnd, soundEffectPos.yEnd)
+  
+
+  const startEffect = soundEffectPos.x - 25 + (soundEffectPos.xEnd * state.soundEffect / 100)
+  ctx.fillRect(startEffect, soundEffectPos.y - 20, 50, 50)
+
 
   ctx.fillText("Music Volume", gameWidth / 2 - 280, 625)
   ctx.fillRect(soundEffectPos.x, soundEffectPos.y + 200, soundEffectPos.xEnd, soundEffectPos.yEnd)
+
+  if (state.current.timeTillNextInput <= 0) {
+    if (state.current.selected == 0) {
+
+    }
+    menuSelectMove(2)
+  }
+
   
+
+  if (state.current.timeTillNextInput >= 1) {
+    state.current.timeTillNextInput -= 1
+  }
+
   ctx.fillText("Back", gameWidth / 2 - 110, 825)
 }
